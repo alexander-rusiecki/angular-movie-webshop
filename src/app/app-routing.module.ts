4260c26c65ab3cec83c7d8514c7520234876f853
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2022-02-27 20:14:35 +0100

    add components and basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MoviesComponent } from '@components/movies/movies.component';
+import { CartComponent } from '@components/cart/cart.component';
+import { MovieDetailsComponent } from '@components/movie-details/movie-details.component';
+import { NotFoundComponent } from '@components/not-found/not-found.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: MoviesComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'movies/:id', component: MovieDetailsComponent },
+  { path: '**', component: NotFoundComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CartComponent } from './components/cart/cart.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent, CartComponent, NotFoundComponent, MovieDetailsComponent, MoviesComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
