0a1c570020909d7509e128a7bb6b0bf8dfd5ff0b
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2022-03-11 01:37:28 +0100

    initialize app with empty cart in local storage if no cart found

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { LocalStorageService } from './services/local-storage.service';
+import { Component, OnInit } from '@angular/core';
+import { IMovie } from '@interfaces/MovieInterface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title: string = 'Angular movie webshop';
+  moviesInCart: IMovie[] = [];
+  constructor(private localStorageService: LocalStorageService) {}
+  ngOnInit(): void {
+    this.moviesInCart = this.localStorageService.get('boughtMovies');
+  }
 }
